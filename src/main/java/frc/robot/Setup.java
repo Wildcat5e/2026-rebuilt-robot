3e3ac5db7f20862f8efd7d1a361a913c359b530d
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2026-01-17 13:59:32 -0500

    rename and update javadocs for max speed constants also javadocs for motor id tests

--- src/main/java/frc/robot/Setup.java
+++ src/main/java/frc/robot/Setup.java
@@ -15,18 +15,18 @@ import frc.robot.subsystems.Drivetrain;
 /** Used for Robot Setup. Lots of static methods and variables */
 class Setup {
     /** kSpeedAt12Volts desired top speed */
-    static final double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
-    /** 3/4 of a rotation per second max angular velocity */
-    static final double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
+    static final double MAX_LINEAR_SPEED = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+    /** 3/4 revs per sec max angular velocity in radians per second */
+    static final double MAX_ANGULAR_SPEED = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
 
     /** The only instance of Drivetrain. */
     static final Drivetrain drivetrain = TunerConstants.createDrivetrain();
     /** The only instance of the Xbox Controller. */
     static final CommandXboxController joystick = new CommandXboxController(0);
     /** Setting up bindings for necessary control of the swerve drive platform */
-    static final SwerveRequest.FieldCentric drive =
-        new SwerveRequest.FieldCentric().withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    static final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+        .withDeadband(MAX_LINEAR_SPEED * 0.1).withRotationalDeadband(MAX_ANGULAR_SPEED * 0.1) // Add a 10% deadband
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
 
 
     /** Dashboard field widget */
@@ -42,15 +42,22 @@ class Setup {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MAX_LINEAR_SPEED) // Drive forward with negative Y (forward)
+                .withVelocityY(-joystick.getLeftX() * MAX_LINEAR_SPEED) // Drive left with negative X (left)
+                .withRotationalRate(-joystick.getRightX() * MAX_ANGULAR_SPEED) // Drive counterclockwise with negative X (left)
             ));
 
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        /*
+         * Tests for motor identification:
+         * https://docs.wpilib.org/en/stable/docs/software/advanced-controls/system-identification/creating-routine.html
+         * https://v6.docs.ctr-electronics.com/en/stable/docs/api-reference/wpilib-integration/sysid-integration
+         */
+        // Quasistatic test for motor identification
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        // Dynamic test for motor identification
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
     }
 
     /**
