75d521dada9557fe73d47a996e1b1a57f533acf5
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2026-01-18 15:42:39 -0500

    add javadocs to `ControllerWrapper.java`

--- src/main/java/frc/robot/subsystems/ControllerWrapper.java
+++ src/main/java/frc/robot/subsystems/ControllerWrapper.java
@@ -3,16 +3,27 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
+/**
+ * Interface for standardized Controller use.
+ * 
+ * @apiNote Each axis uses the
+ *          <a href="https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html">WPILib
+ *          coordinate system<a>.
+ */
 public interface ControllerWrapper {
+    /** Get the X axis value. @return The axis value. */
     public double getX();
 
+    /** Get the Y axis value. @return The axis value. */
     public double getY();
 
+    /** Get the rotation axis value. @return The axis value. */
     public double getRotation();
 
     static class Xbox implements ControllerWrapper {
         private final XboxController controller;
 
+        /** Uses {@link XboxController}. @param port index on Driver Station */
         public Xbox(int port) {
             controller = new XboxController(port);
         }
@@ -36,6 +47,7 @@ public interface ControllerWrapper {
     static class LogitechFlightStick implements ControllerWrapper {
         private final Joystick controller;
 
+        /** Uses {@link Joystick} for Logitech Extreme 3D Pro. @param port index on Driver Station */
         public LogitechFlightStick(int port) {
             controller = new Joystick(port);
         }
