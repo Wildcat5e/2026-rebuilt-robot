813ba6671613018c682860674c6b98903e00810a
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2026-01-24 16:15:16 -0500

    add idle on disable

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.commands.*;
 import frc.robot.generated.TunerConstants;
@@ -139,6 +140,11 @@ public class Robot extends TimedRobot {
             }
             return Robot.swerveRequest;
         }));
+        // Idle while the robot is disabled. This ensures the configured neutral mode is applied to the drive motors while disabled.
+        final SwerveRequest.Idle idle = new SwerveRequest.Idle();
+        final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+        RobotModeTriggers.disabled().whileTrue(drivetrain.applyRequest(() -> idle).ignoringDisable(true));
+
         // reset the field-centric heading on left trigger
         Controller.joystick.leftTrigger().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
         Controller.joystick.a().whileTrue(rotateToHub);
