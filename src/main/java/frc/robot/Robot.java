e9489e1b4db673aeba53686a4825169d60d3945b
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2026-01-31 21:26:43 -0500

    eliminate `RobotContainer`

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,21 @@
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathfindingCommand;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.StringTopic;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.RotateToHub;
+import frc.robot.subsystems.Controller;
+import frc.robot.subsystems.PhotonVision;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in the TimedRobot
@@ -8,21 +23,43 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
+    /** The only instance of the Controller. */
+    final Controller controller = new Controller.Xbox(0);
+    /** The only instance of PhotonVision. */
+    final PhotonVision photonVision = new PhotonVision(Controller.drivetrain::addVisionMeasurement);
+    /** Dashboard field widget */
+    final Field2d field = new Field2d();
+    /** A chooser for autonomous commands */
+    final SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
+    /** https://github.com/Gold872/elastic_dashboard/blob/v2026.1.1/elasticlib/Elastic.java */
+    public final StringTopic elasticTabTopic = NetworkTableInstance.getDefault().getStringTopic("/Elastic/SelectedTab");
+    public final StringPublisher elasticTabPublisher = elasticTabTopic.publish(PubSubOption.keepDuplicates(true));
+
     /** This function is run when the robot is first started up and should be used for any initialization code. */
     public Robot() {
-        RobotContainer.generalSetup();
+        CommandScheduler.getInstance().schedule(PathfindingCommand.warmupCommand()); // replaces: PathfindingCommand.warmupCommand().schedule();
+        SignalLogger.enableAutoLogging(false);
+        controller.bindingsSetup();
+        SmartDashboard.putData("Field", field);
+        SmartDashboard.putData("Command Scheduler", CommandScheduler.getInstance());
+        SmartDashboard.putData("Auto Command Chooser", autoChooser);
+        SmartDashboard.putData("RotateToHub PID Controller", RotateToHub.PID_CONTROLLER);
+        SmartDashboard.putBoolean("Within Shooting Angle", Utilities.withinShootingAngle());
+        SmartDashboard.putBoolean("Within Shooting Distance", Utilities.withinShootingDistance());
     }
 
     @Override
     public void robotPeriodic() {
-        RobotContainer.runCommands();
-        RobotContainer.dashboardUpdate();
+        CommandScheduler.getInstance().run();
+        field.setRobotPose(Controller.drivetrain.getState().Pose);
     }
 
     @Override
     public void autonomousInit() {
-        RobotContainer.elasticTabPublisher.set(RobotContainer.ELASTIC_AUTONOMOUS);
-        RobotContainer.runAuto();
+        elasticTabPublisher.set("Autonomous");
+        if (autoChooser.getSelected() != null) {
+            CommandScheduler.getInstance().schedule(autoChooser.getSelected());
+        }
     }
 
     @Override
@@ -30,8 +67,10 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        RobotContainer.elasticTabPublisher.set(RobotContainer.ELASTIC_TELEOP);
-        RobotContainer.cancelAuto();
+        elasticTabPublisher.set("Teleoperated");
+        if (autoChooser.getSelected() != null) {
+            CommandScheduler.getInstance().cancel(autoChooser.getSelected());
+        }
     }
 
     @Override
