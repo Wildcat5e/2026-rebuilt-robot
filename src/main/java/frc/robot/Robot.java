6dc59de8b00a7bc79bb879de414e8e82bf5f2bd1
wildcat6705 <147210783+wildcat6705@users.noreply.github.com>
2026-02-03 18:07:34 -0500

    pull `configureAutoBuilder()` into `Robot` out of `Drivetrain`

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,13 +1,20 @@
 package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathfindingCommand;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.PubSubOption;
 import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.networktables.StringTopic;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -40,7 +47,7 @@ public class Robot extends TimedRobot {
     /** This function is run when the robot is first started up and should be used for any initialization code. */
     public Robot() {
         NamedCommands.registerCommand("Rotate To Hub", RobotCommands.rotateToHub);
-        Controller.drivetrain.configureAutoBuilder();
+        configureAutoBuilder();
         autoChooser = AutoBuilder.buildAutoChooser();
         CommandScheduler.getInstance().schedule(PathfindingCommand.warmupCommand()); // replaces: PathfindingCommand.warmupCommand().schedule();
         SignalLogger.enableAutoLogging(false);
@@ -104,4 +111,34 @@ public class Robot extends TimedRobot {
     public void simulationPeriodic() {
         simulation.poseUpdate();
     }
+
+    public void configureAutoBuilder() {// @formatter:off
+        try {
+            var applyRobotSpeedsRequest = new SwerveRequest.ApplyRobotSpeeds();
+            var config = RobotConfig.fromGUISettings();
+            AutoBuilder.configure(
+                () -> Controller.drivetrain.getState().Pose,   // Supplier of current robot pose
+                Controller.drivetrain::resetPose,         // Consumer for seeding pose against auto
+                () -> Controller.drivetrain.getState().Speeds, // Supplier of current robot speeds
+                // Consumer of ChassisSpeeds and feedforwards to drive the robot
+                (speeds, feedforwards) -> Controller.drivetrain.setControl(
+                    applyRobotSpeedsRequest.withSpeeds(ChassisSpeeds.discretize(speeds, 0.020))
+                        .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+                        .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
+                ),
+                new PPHolonomicDriveController(
+                    // PID constants for translation
+                    new PIDConstants(10, 0, 0),
+                    // PID constants for rotation
+                    new PIDConstants(7, 0, 0)
+                ),
+                config,
+                // Assume the path needs to be flipped for Red vs Blue, this is normally the case
+                () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
+                Controller.drivetrain // Subsystem for requirements
+            );
+        } catch (Exception ex) {
+            DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
+        }
+    }// @formatter:on
 }
