b2cd03b3da33cdbf82ca055a25a3a414f541078d
Adi Z <ztech58@gmail.com>
2026-03-07 10:39:08 -0500

    Add `applyGearRatio(motor, gearRatio)` to `Utilities`

--- src/main/java/frc/robot/Utilities.java
+++ src/main/java/frc/robot/Utilities.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -45,5 +47,29 @@ public interface Utilities {
         // If the Robot is on the Red Alliance, return whether it's to the right of the Red Home Zone threshold.
         return Robot.isBlueAlliance ? robotXPosition < Constants.BLUE_X_AXIS_HOME_THRESHOLD
             : robotXPosition > Constants.RED_X_AXIS_HOME_THRESHOLD;
+    }
+
+    /**
+     * Applies a gear ratio to a TalonFX motor so that getPosition() and getVelocity() automatically return mechanism
+     * rotations instead of motor rotor rotations.
+     * 
+     * @param motor The TalonFX motor to configure.
+     * @param gearRatio The gear ratio.<br>
+     *        NOTE ON THE RATIO VALUE:<br>
+     *        If the motor spins 10 times to spin the mechanism 1 time (a standard 10:1 reduction), the gear ratio is
+     *        10.0. A gear ratio of 0.5 would be an overdrive where the motor spins half a turn to spin the mechanism a
+     *        full turn.
+     */
+    static void applyGearRatio(TalonFX motor, double gearRatio) {
+        TalonFXConfiguration config = new TalonFXConfiguration();
+
+        // Fetch the motor's current configuration so we don't overwrite other settings
+        motor.getConfigurator().refresh(config);
+
+        // Modify the SensorToMechanismRatio
+        config.Feedback.SensorToMechanismRatio = gearRatio;
+
+        // Apply the updated configuration back to the motor
+        motor.getConfigurator().apply(config);
     }
 }
