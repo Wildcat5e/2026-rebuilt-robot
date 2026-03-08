20a368e56a1ed9a6980b91e32997049c28d77d8e
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2026-03-07 22:45:28 -0500

    Invert kicker, sane default, remove comments

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -15,7 +15,7 @@ public class Hopper extends SubsystemBase {
         applyGearRatio(leftHopperMotor, 0.5);
         applyGearRatio(rightHopperMotor, 0.5);
         applyGearRatio(kickerMotor, 0.5);
-        SmartDashboard.putNumber("Kicker Test Voltage", 0.0);
+        SmartDashboard.putNumber("Kicker Test Voltage", 8);
     }
 
     @Override
@@ -44,18 +44,11 @@ public class Hopper extends SubsystemBase {
         return startEnd(() -> kickerMotor.setVoltage(-8), () -> kickerMotor.setVoltage(0));
     }
 
-    /**
-     * Reads the "Kicker Test Voltage" from SmartDashboard and applies it to the kicker motor.
-     */
     public Command testTunableKicker() {
-        return runEnd(
-            // --execute-- (runs continuously)
-            () -> {
-                double targetVoltage = SmartDashboard.getNumber("Kicker Test Voltage", 0.0);
-                kickerMotor.setVoltage(targetVoltage);
-            },
-            // --onEnd-- (runs when interrupted or finished)
-            () -> kickerMotor.setVoltage(0));
+        return runEnd(() -> {
+            double targetVoltage = SmartDashboard.getNumber("Kicker Test Voltage", 0.0);
+            kickerMotor.setVoltage(-targetVoltage);
+        }, () -> kickerMotor.setVoltage(0));
     }
 
     public void runFeeder() {
