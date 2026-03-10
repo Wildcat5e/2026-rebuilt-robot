ac27d4b7fc0d3759a22f3b21a06409a61435b8b2
wildcat6705 <147210783+wildcat6705@users.noreply.github.com>
2026-03-10 19:06:51 -0400

    Add test commands for all motors on intake, add suppliers smartdashboard values for voltages

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,26 +4,45 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.DashboardManager;
 import static frc.robot.Utilities.*;
 
 public class Intake extends SubsystemBase {
-    /** Motor that connects to actual wheels to intake fuel into storage. */
-    private final TalonFX conveyorMotor = new TalonFX(0);
+    /** Motor that is closer to the floor and scoops fuel into other set of wheels. */
+    private final TalonFX scooperMotor = new TalonFX(0);
+    /** Motor that shoots fuel into the robot */
+    private final TalonFX pusherMotor = new TalonFX(0);
     /** Motor that extends intake system outside of bumper. */
     private final TalonFX extenderMotor = new TalonFX(0);
+    private double extenderMotorPosition = 0;
 
     public Intake() {
-        applyGearRatio(conveyorMotor, 0.5);
-        applyGearRatio(extenderMotor, 0.5);
+        // GEAR RATIO IS 36, BUT MECHANICAL RATIO IS 48
+        applyGearRatio(extenderMotor, 36);
+        DashboardManager.setupIntake(() -> extenderMotorPosition);
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        extenderMotorPosition = getExtenderPosition();
+    }
 
-    public Command dropArm() {
+    public Command spinIntakeMotors() {
+        return startEnd(() -> {
+            double scooperMotorVoltage = DashboardManager.getScooperMotorTestVoltage();
+            double pusherMotorVoltage = DashboardManager.getPusherMotorTestVoltage();
+            scooperMotor.setVoltage(scooperMotorVoltage);
+            pusherMotor.setVoltage(pusherMotorVoltage);
+        }, () -> {
+            scooperMotor.setVoltage(0);
+            pusherMotor.setVoltage(0);
+        });
+    }
+
+    public Command dropArmFinalImplementation() {
         return new FunctionalCommand(
             // --initialize--
-            () -> extenderMotor.setVoltage(3), // If this doesn't work, might have to put setVoltage() inside execute
+            () -> extenderMotor.setVoltage(3),
 
             // --execute--
             () -> {},
@@ -35,7 +54,6 @@ public class Intake extends SubsystemBase {
             () -> {
                 return getExtenderPosition() >= .3; // Check sign
             },
-
             // --addRequirements--
             this); // Pass in Intake
     }
@@ -44,33 +62,28 @@ public class Intake extends SubsystemBase {
      * TURN ON INTAKE TO TAKE IN FUEL COMMAND <br>
      * Currently plan to bind to a while true button, but may be easier to have it run on toggle or for the entire match
      */
-    public Command testFuelIntake() {
-        return startEnd(
-            // --onStart--
-            () -> conveyorMotor.setVoltage(4),
-            // --onEnd--
-            () -> conveyorMotor.setVoltage(0));
+    public Command testScooper() {
+        return startEnd(() -> {
+            double scooperMotorVoltage = DashboardManager.getScooperMotorTestVoltage();
+            scooperMotor.setVoltage(scooperMotorVoltage);
+        }, () -> scooperMotor.set(0));
     }
 
-    // Untested
-    public Command turnArmClockwise() {
-        return startEnd(
-            // --onStart--
-            () -> extenderMotor.setVoltage(3),
-            // --onEnd--
-            () -> extenderMotor.setVoltage(0));
+    public Command testPusher() {
+        return startEnd(() -> {
+            double pusherMotorVoltage = DashboardManager.getPusherMotorTestVoltage();
+            pusherMotor.setVoltage(pusherMotorVoltage);
+        }, () -> pusherMotor.set(0));
     }
 
-    // Untested
-    public Command turnArmCounterClockwise() {
-        return startEnd(
-            // --onStart--
-            () -> extenderMotor.setVoltage(-3),
-            // --onEnd--
-            () -> extenderMotor.setVoltage(0));
+    public Command testExtender() {
+        return startEnd(() -> {
+            double extenderMotorVoltage = DashboardManager.getExtenderMotorTestVoltage();
+            extenderMotor.setVoltage(extenderMotorVoltage);
+        }, () -> extenderMotor.setVoltage(0));
     }
 
-    /** @return Extender motor's position in rotations */
+    /** @return Extender motor's position in REVOLUTIONS */
     public double getExtenderPosition() {
         return extenderMotor.getPosition().getValueAsDouble();
     }
