4b028678a99c878931b56a42a1f7b8ee74277c17
Adi Z <ztech58@gmail.com>
2026-03-11 03:22:27 -0400

    Remove unused `SmartDashboard` imports

--- src/main/java/frc/robot/commands/RotateToHub.java
+++ src/main/java/frc/robot/commands/RotateToHub.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.robot.subsystems.Controller;
