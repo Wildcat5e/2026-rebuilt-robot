d2cac7d6d3c2c6976aa2276980f2255a94ca0558
Adi Z <ztech58@gmail.com>
2026-03-12 23:07:34 -0400

    Add radii, circumferences, and sysId routines for kicker and conveyor motors
    Also reordered the various methods to first have kicker, and then conveyor, because that's the order that the motor IDs are in. This should be standard across all files, if possible. Makes it easy to find things if we have to debug quickly. Later, we can create a reference manual for motor IDs.

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -1,31 +1,47 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.DashboardManager;
 import static frc.robot.Utilities.*;
 
 public class Hopper extends SubsystemBase {
-    private final TalonFX conveyorMotor = new TalonFX(15);
     private final TalonFX kickerMotor = new TalonFX(14);
+    private final double KICKER_RADIUS = 0.03175 / 2;
+    private final double KICKER_CIRCUMFERENCE = 2 * Math.PI * KICKER_RADIUS;
+
+    private final TalonFX conveyorMotor = new TalonFX(15);
+    private final double CONVEYOR_RADIUS = 0.028575 / 2;
+    private final double CONVEYOR_CIRCUMFERENCE = 2 * Math.PI * CONVEYOR_RADIUS;
+
+    // --- SysId Configuration ---
+    // ONLY UNCOMMENT THE ROUTINE CREATION LINE FOR THE MOTOR YOU WANT TO CHARACTERIZE.
+    // OTHERWISE, YOU RISK DAMAGING YOUR ROBOT.
+    // MAKE SURE TO COMMENT OUT THE OTHER TWO BEFORE RUNNING ANY CHARACTERIZATION COMMANDS.
+
+    // --KICKER MOTOR--
+    private final SysIdRoutine routine =
+        createLinearRoutine(this, kickerMotor, kickerMotor::setVoltage, KICKER_CIRCUMFERENCE);
+
+    // --CONVEYOR MOTOR--
+    // private final SysIdRoutine routine =
+    //     createLinearRoutine(this, conveyorMotor, conveyorMotor::setVoltage, CONVEYOR_CIRCUMFERENCE);
+
+    /** @return The SysIdRoutine used to generate characterization commands. */
+    public SysIdRoutine getSysIdRoutine() {
+        return routine;
+    }
 
     public Hopper() {
-        applyGearRatio(conveyorMotor, 4);
         applyGearRatio(kickerMotor, 4);
+        applyGearRatio(conveyorMotor, 4);
         DashboardManager.setupHopper();
     }
 
     @Override
     public void periodic() {}
-
-    public Command testConveyor() {
-        return startEnd(() -> {
-            double targetConveyorVoltage = DashboardManager.getConveyorTestVoltage();
-            conveyorMotor.setVoltage(-targetConveyorVoltage);
-        }, () -> conveyorMotor.setVoltage(0));
-    }
 
     /** Reads the "Kicker Test Voltage" from SmartDashboard and applies it continuously. */
     public Command testTunableKicker() {
@@ -35,13 +51,20 @@ public class Hopper extends SubsystemBase {
         }, () -> kickerMotor.setVoltage(0));
     }
 
+    public Command testConveyor() {
+        return startEnd(() -> {
+            double targetConveyorVoltage = DashboardManager.getConveyorTestVoltage();
+            conveyorMotor.setVoltage(-targetConveyorVoltage);
+        }, () -> conveyorMotor.setVoltage(0));
+    }
+
     public void runFeeder() {
-        conveyorMotor.setVoltage(3);
         kickerMotor.setVoltage(-8);
+        conveyorMotor.setVoltage(3);
     }
 
     public void stopFeeder() {
-        conveyorMotor.setVoltage(0);
         kickerMotor.setVoltage(0);
+        conveyorMotor.setVoltage(0);
     }
 }
