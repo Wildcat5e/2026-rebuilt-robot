4730afdf59666288b331efe450f130017db829d1
EthanYLeong <ethanleong26@yahoo.com>
2026-03-13 09:58:09 -0400

    Add pusher to shootFuel command, add smartdashboard to shootFuel methods

--- src/main/java/frc/robot/commands/RobotCommands.java
+++ src/main/java/frc/robot/commands/RobotCommands.java
@@ -8,13 +8,13 @@ import frc.robot.subsystems.Intake;
 public class RobotCommands {
     public final RotateToHub rotateToHub;
     public final RotateToHub rotateToHubShootingCalc;
-    public final Paths paths;
+    // public final Paths paths;
     public final ShootFuel shootFuel;
 
-    public RobotCommands(Drivetrain drivetrain, Flywheel flywheel, Hopper hopper) {
+    public RobotCommands(Drivetrain drivetrain, Flywheel flywheel, Intake intake, Hopper hopper) {
         rotateToHub = new RotateToHub(drivetrain, false);
         rotateToHubShootingCalc = new RotateToHub(drivetrain, true);
-        paths = new Paths(drivetrain);
-        shootFuel = new ShootFuel(flywheel, hopper, drivetrain);
+        // paths = new Paths(drivetrain);
+        shootFuel = new ShootFuel(flywheel, hopper, intake, drivetrain);
     }
 }
--- src/main/java/frc/robot/commands/ShootFuel.java
+++ src/main/java/frc/robot/commands/ShootFuel.java
@@ -5,17 +5,20 @@ import frc.robot.Utilities;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Flywheel;
 import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Intake;
 
 public class ShootFuel extends Command {
     Flywheel flywheel;
     Hopper hopper;
+    Intake intake;
     Drivetrain drivetrain;
 
-    public ShootFuel(Flywheel flywheel, Hopper hopper, Drivetrain drivetrain) {
+    public ShootFuel(Flywheel flywheel, Hopper hopper, Intake intake, Drivetrain drivetrain) {
         this.flywheel = flywheel;
         this.hopper = hopper;
+        this.intake = intake;
         this.drivetrain = drivetrain;
-        addRequirements(flywheel, hopper);
+        addRequirements(flywheel, hopper, intake);
     }
 
     @Override
@@ -25,19 +28,25 @@ public class ShootFuel extends Command {
     public void execute() {
         if (Utilities.inHome(drivetrain)) {
             flywheel.dynamicRunFlywheel();
+            System.out.println("DYNAMIC !!!");
         } else {
             flywheel.staticRunFlywheel();
+            System.out.println("STATIC !!!!");
         }
 
         if (flywheel.flywheelUpToSpeed()) {
-            hopper.runFeeder();
+            System.out.println(" UP TO SPEED ");
+            hopper.runHopper();
+            intake.spinPusher();
         }
     }
 
     @Override
     public void end(boolean interrupted) {
         flywheel.stopFlywheel();
-        hopper.stopFeeder();
+        hopper.stopHopper();
+        intake.stopPusher();
+        System.out.println("DONE");
     }
 
     @Override
