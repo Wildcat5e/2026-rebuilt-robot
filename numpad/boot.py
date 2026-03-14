176b32cf3ea21d6ca9a1853070481ebc166f36af
Adi Z <ztech58@gmail.com>
2026-03-14 13:51:18 -0400

    Update `boot.py` to support 32 buttons

--- numpad/boot.py
+++ numpad/boot.py
@@ -1,6 +1,5 @@
 import usb_hid
 
-# This is only one example of a gamepad descriptor, and may not suit your needs.
 GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x05, 0x01,  # Usage Page (Generic Desktop Ctrls)
     0x09, 0x05,  # Usage (Game Pad)
@@ -8,11 +7,11 @@ GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x85, 0x04,  #   Report ID (4)
     0x05, 0x09,  #   Usage Page (Button)
     0x19, 0x01,  #   Usage Minimum (Button 1)
-    0x29, 0x10,  #   Usage Maximum (Button 16)
+    0x29, 0x20,  #   Usage Maximum (Button 32)
     0x15, 0x00,  #   Logical Minimum (0)
     0x25, 0x01,  #   Logical Maximum (1)
     0x75, 0x01,  #   Report Size (1)
-    0x95, 0x10,  #   Report Count (16)
+    0x95, 0x20,  #   Report Count (32)
     0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
     0x15, 0x81,  #   Logical Minimum (-127)
@@ -32,7 +31,7 @@ gamepad = usb_hid.Device(
     usage_page=0x01,           # Generic Desktop Control
     usage=0x05,                # Gamepad
     report_ids=(4,),           # Descriptor uses report ID 4.
-    in_report_lengths=(6,),    # This gamepad sends 6 bytes in its report.
+    in_report_lengths=(8,),
     out_report_lengths=(0,),   # It does not receive any reports.
 )
 
