259883c1209c86be80b8acca875e4c1e4a48a265
Adi Z <ztech58@gmail.com>
2026-03-14 12:21:05 -0400

    Change back to single tag and 15 static flywheel

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -39,7 +39,7 @@ public class PhotonVision extends SubsystemBase {
     public void periodic() {
         Optional<EstimatedRobotPose> visionEst = Optional.empty();
         for (PhotonPipelineResult result : CAMERA_2.getAllUnreadResults()) {
-            visionEst = ESTIMATOR.estimateCoprocMultiTagPose(result);
+            visionEst = ESTIMATOR.estimateLowestAmbiguityPose(result);
             if (visionEst.isEmpty()) {
                 visionEst = ESTIMATOR.estimateLowestAmbiguityPose(result);
             }
