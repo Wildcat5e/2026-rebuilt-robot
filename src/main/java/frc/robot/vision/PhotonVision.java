8435f16750203816aa7c54f243e54383823386a9
wildcat6705 <147210783+wildcat6705@users.noreply.github.com>
2026-03-19 17:42:41 -0400

    Fix big where multi stddev would be used for single april tag > 4 meters away

--- src/main/java/frc/robot/vision/PhotonVision.java
+++ src/main/java/frc/robot/vision/PhotonVision.java
@@ -23,44 +23,47 @@ import static java.util.stream.Collectors.toMap;
 public class PhotonVision extends SubsystemBase {
     // Standard deviations to weight vision pose updates. (Higher values weight vision less.)
     public static final Matrix<N3, N1> SINGLE_TAG_STD_DEV = VecBuilder.fill(4, 4, 8);
-    public static final Matrix<N3, N1> MULTI_TAG_STD_DEV  = VecBuilder.fill(0.5, 0.5, 1);
+    public static final Matrix<N3, N1> MULTI_TAG_STD_DEV = VecBuilder.fill(0.5, 0.5, 1);
 
-    private final PhotonCamera         photonCamera;
-    private final PhotonPoseEstimator  poseEstimator;
+    private final PhotonCamera photonCamera;
+    private final PhotonPoseEstimator poseEstimator;
     private final PoseEstimateConsumer poseEstimateConsumer;
     private final Map<Integer, Pose2d> aprilTagPoses;
 
     /**
-     * @param camera           The camera to use for pose estimation. Make sure to set the correct camera name and robot-to-camera transform.
-     * @param estimator        The photon pose estimator used for pose estimation.
+     * @param camera The camera to use for pose estimation. Make sure to set the correct camera name and robot-to-camera
+     *        transform.
+     * @param estimator The photon pose estimator used for pose estimation.
      * @param estimateConsumer The consumer receiving the estimated pose and timestamp.
      */
     public PhotonVision(PhotonCamera camera, PhotonPoseEstimator estimator, PoseEstimateConsumer estimateConsumer) {
         super(camera.getName());
-        this.photonCamera         = camera;
+        this.photonCamera = camera;
         this.poseEstimateConsumer = estimateConsumer;
-        this.poseEstimator        = estimator;
+        this.poseEstimator = estimator;
         // Only convert AprilTags to poses once since the field layout is static. Map tag ID to pose for fast lookup during pose estimation.
+        // @formatter:off
         this.aprilTagPoses = estimator.getFieldTags()
                                       .getTags()
                                       .stream()
-                                      .collect(toMap(tag -> tag.ID, tag -> tag.pose.toPose2d()));
+                                      .collect(toMap(tag -> tag.ID, tag -> tag.pose.toPose2d())); // @formatter:on
     }
 
 
     public PhotonVision(PoseEstimateConsumer estimateConsumer) {
         this(new PhotonCamera("HD_Pro_Webcam_C920"),
-             new PhotonPoseEstimator(AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded),
-                                     new Transform3d(-0.07, .295, .57, Rotation3d.kZero)),
-             estimateConsumer);
+            new PhotonPoseEstimator(AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded),
+                new Transform3d(-0.07, .295, .57, Rotation3d.kZero)),
+            estimateConsumer);
     }
 
-    @Override public void periodic() {
+    @Override
+    public void periodic() {
         for (PhotonPipelineResult result : photonCamera.getAllUnreadResults()) {
 
             // Use coprocessor if available, otherwise use lowest ambiguity pose.
             var optionalPoseEstimate = poseEstimator.estimateCoprocMultiTagPose(result)
-                                                    .or(() -> poseEstimator.estimateLowestAmbiguityPose(result));
+                .or(() -> poseEstimator.estimateLowestAmbiguityPose(result));
 
             if (optionalPoseEstimate.isEmpty()) {
                 // No valid pose estimate for this result, skip it.
@@ -68,41 +71,42 @@ public class PhotonVision extends SubsystemBase {
             }
 
             // Convert targets to April Tag 2d poses, removing null values
+            // @formatter:off
             var tagPoses = result.targets.stream()
                                          .map(PhotonTrackedTarget::getFiducialId)
                                          .map(aprilTagPoses::get)
                                          .filter(Objects::nonNull)
-                                         .toList();
+                                         .toList(); // @formatter:on
 
             if (tagPoses.isEmpty()) {
                 // No valid April Tag poses for the targets in this result, skip it since we can't calculate distances to tags.
                 continue;
             }
 
-            var poseEstimate   = optionalPoseEstimate.get();
+            var poseEstimate = optionalPoseEstimate.get();
             var poseEstimate2d = poseEstimate.estimatedPose.toPose2d();
 
             // Average distance between tag pose and robot pose estimate.
             // This gives us a rough idea of how far the robot is from the tags it sees,
             // which is a major factor in pose estimation accuracy. We use this to adjust our
             // confidence in the pose estimate (i.e. the standard deviation we pass to the consumer).
-            var averageDistance = tagPoses.stream()
-                                          .map(tagPose -> tagPose.getTranslation()
-                                                                 .getDistance(poseEstimate2d.getTranslation()))
-                                          .reduce(0.0, Double::sum) / tagPoses.size();
+            var averageDistance =
+                tagPoses.stream().map(tagPose -> tagPose.getTranslation().getDistance(poseEstimate2d.getTranslation()))
+                    .reduce(0.0, Double::sum) / tagPoses.size();
 
-            if (tagPoses.size() == 1 && averageDistance <= 4.0) {
-                // Only use single tag pose estimate if the average distance is less than 4 meters.
-                // This is an empirical threshold based on testing that balances trusting single tag estimates
-                // when they are likely accurate and ignoring them when they are likely inaccurate.
-                poseEstimateConsumer.accept(poseEstimate2d, poseEstimate.timestampSeconds, SINGLE_TAG_STD_DEV);
-            } else {
+
+            if (tagPoses.size() > 1) {
                 // For multiple tags, we can be more confident in the pose estimate, so we use a lower standard deviation.
                 // However, we still want to account for distance to the tags, since farther tags generally lead to less
                 // accurate estimates. This heuristic scales the standard deviation based on the average distance to the tags,
                 // with a cap at around 4 meters (since beyond that, vision is generally not very reliable).
                 var standardDeviation = MULTI_TAG_STD_DEV.times(1 * (averageDistance * averageDistance / 30.0));
                 poseEstimateConsumer.accept(poseEstimate2d, poseEstimate.timestampSeconds, standardDeviation);
+            } else if (averageDistance <= 4.0) {
+                // Only use single tag pose estimate if the average distance is less than 4 meters.
+                // This is an empirical threshold based on testing that balances trusting single tag estimates
+                // when they are likely accurate and ignoring them when they are likely inaccurate.
+                poseEstimateConsumer.accept(poseEstimate2d, poseEstimate.timestampSeconds, SINGLE_TAG_STD_DEV);
             }
         }
     }
