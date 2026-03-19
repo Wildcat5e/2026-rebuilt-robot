c79568257addaaea70804399905be150cae0e473
Adi Z <ztech58@gmail.com>
2026-03-19 16:38:13 -0400

    Clean up `ProximitySensor`

--- src/main/java/frc/robot/subsystems/ProximitySensor.java
+++ src/main/java/frc/robot/subsystems/ProximitySensor.java
@@ -7,28 +7,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ProximitySensor {
     public ProximitySensor() {
-        //Initialize Device
-        //The device's default CAN is 0. Change it using AndyMark CAN interface utility
+        // The device's default CAN is 0. Change it using AndyMark CAN interface utility
         AM_CAN_Color_Sensor colorSensor = new AM_CAN_Color_Sensor(0);
-        //Reset Report Period to the default of 100ms
+        // Reset Report Period to the default of 100ms
         colorSensor.resetReportPeriod();
 
-        //UPDATE DEVICE FIRMWARE TO 1.1.1 AND ANDYMARK WPILIB 
-        //VERSION TO 2026.0.2 OR NEWER TO TURN LED ON
-        //Turn on LED for better data in low light conditions
+        // UPDATE DEVICE FIRMWARE TO 1.1.1 AND ANDYMARK WPILIB 
+        // VERSION TO 2026.0.2 OR NEWER TO TURN LED ON
+        // Turn on LED for better data in low light conditions
         colorSensor.turnLedOn();
 
-        //Get data and normalize it using clearC value
+        // Get data and normalize it using clearC value
         AM_ColorSensorData d = colorSensor.getData();
         double red = (double) d.red / d.clearC;
         double blue = (double) d.blue / d.clearC;
         double green = (double) d.green / d.clearC;
-        //Put data on the smart dashboard
-        SmartDashboard.putNumber("Color/Red", red);
-        SmartDashboard.putNumber("Color/Green", green);
-        SmartDashboard.putNumber("Color/Blue", blue);
-        SmartDashboard.putNumber("Color/Clear", d.clearC);
-        SmartDashboard.putNumber("Color/Proximity", d.proximity);
-        SmartDashboard.putNumber("Color/Timestamp(ms)", d.millisStamp);
+
+        SmartDashboard.putNumber("Color Sensor/Red", red);
+        SmartDashboard.putNumber("Color Sensor/Green", green);
+        SmartDashboard.putNumber("Color Sensor/Blue", blue);
+        SmartDashboard.putNumber("Color Sensor/Clear", d.clearC);
+        SmartDashboard.putNumber("Color Sensor/Proximity", d.proximity);
+        SmartDashboard.putNumber("Color Sensor/Timestamp(ms)", d.millisStamp);
     }
 }
