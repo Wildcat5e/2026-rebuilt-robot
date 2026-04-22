dea3ffe30cf1d4f82c6c5c396bc7ebd63d0c4172
Adi Z <ztech58@gmail.com>
2026-04-21 21:07:58 -0400

    Remove unused import from `Flywheel`

--- src/main/java/frc/robot/subsystems/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -6,7 +6,6 @@ import static frc.robot.utilities.HardwareUtils.*;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.filter.LinearFilter;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
